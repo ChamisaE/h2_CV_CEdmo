f20f7dd17af1a0eeda7670806d88370440cdc01b
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2025-03-31 20:03:11 -0500

    imports and read in

---
+++ img_binarization.py
@@ -0,0 +1,12 @@
+#Chamisa Edmo
+#CV Spring 2025
+#KUID: 2209458
+
+
+#imports below
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#read in provided image
+img = cv2.imread("tools.pgm", cv2.IMREAD_GRAYSCALE)
